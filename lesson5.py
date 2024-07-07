12ded9dca2c3ee651513e0b3324052ded264c053
bucks22 <121617528+bucks22@users.noreply.github.com>
2024-07-07 17:37:30 +0500

    Add files via upload

---
+++ lesson5.py
@@ -0,0 +1,6 @@
+immutable_var = ('b', 'c', 2, False, ['z', 'po', 3, 87545])
+#immutable_var[4][1] = 'waq'
+print('Неизменяемый кортеж:', immutable_var)
+mutable_list = [1, 2, 'frog', 'cow']
+mutable_list[2] = 'house'
+print('Изменяемый список:', mutable_list)
